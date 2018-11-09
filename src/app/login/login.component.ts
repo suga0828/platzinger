9e16db6f25a23a26fa72ab91d69e4096b914686f
suga0828 <alexander.sf0828@gmail.com>
2018-11-09 14:59:28 -0500

    LoginComponent modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+	operation: string = 'login'
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() { }
 
 }
