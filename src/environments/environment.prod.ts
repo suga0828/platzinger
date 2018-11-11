cff33b7ab2fa67f5c5e1fd8661af68755d5323d3
suga0828 <alexander.sf0828@gmail.com>
2018-11-10 21:50:40 -0500

    Firebase instaled and configured

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAP_VfcIqBD4EmE8YC58QEEhyEvngRRC0Q",
+    authDomain: "suga0828-platzinger.firebaseapp.com",
+    databaseURL: "https://suga0828-platzinger.firebaseio.com",
+    projectId: "suga0828-platzinger",
+    storageBucket: "suga0828-platzinger.appspot.com",
+    messagingSenderId: "447776741462"
+  }
 };
