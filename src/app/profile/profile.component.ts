5ecd42dfc311ed004d18a80972ad3452211572f3
suga0828 <alexander.sf0828@gmail.com>
2018-11-14 13:09:07 -0500

    From profileComponent branch: edit user information

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from '../services/user.service';
+import { AuthenticationService } from '../services/authentication.service';
+import { User } from '../interfaces/user';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+
+  constructor(
+    private userService: UserService,
+    private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
+    this.authenticationService.getStatus()
+      .subscribe(
+        status => {
+          this.userService.getUserById(status.uid)
+            .valueChanges()
+            .subscribe(
+              (data: User) => {
+                console.log(data)
+                this.user = data
+              },
+              error => console.log(error)
+            )
+        },
+        error => console.log(error)
+      )
   }
+
+  saveSettings() {
+    console.log(this.user)
+    this.userService.editUser(this.user)
+      .then( () => alert('Cambios guardados correctamente') )
+      .catch( error => {
+        alert('Hubo un error')
+        console.log(error)
+      })
+  }
+
 
 }
